dc2da7cfc6b8cb9a0b468abddc23b34e98fe0c84
IMysch <55532005+IMysch@users.noreply.github.com>
2021-07-22 08:04:39 +0700

    Create reverse.java

---
+++ .github/workflows/reverse.java
@@ -0,0 +1,10 @@
+public class reverse {
+    public static void main(String args[]){
+        
+        StringBuffer kataBalik; //deklarasi var
+        
+        kataBalik = new StringBuffer("Aplikasi");
+        
+        System.out.println(kataBalik.reverse());
+    }
+}

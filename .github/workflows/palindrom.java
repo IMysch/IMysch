5d6d2f772761116b657d00641eb24c8c19dd4a66
IMysch <55532005+IMysch@users.noreply.github.com>
2021-07-22 08:06:29 +0700

    Create palindrom.java

---
+++ .github/workflows/palindrom.java
@@ -0,0 +1,22 @@
+public class Palindrom {
+   
+	public static void main(String[] args){
+		new Palindrom().palindrome("Katak di balik kataK");		
+	}
+	
+	private void palindrome(String teks){		
+		String poli = "";		
+
+		for(int i=teks.length();i>0;i--){
+			String test1 = teks.substring(i-1, i);
+			poli += test1;
+		}
+		
+		if(teks.equals(poli)){
+			System.out.println("==== "+teks+" adalah Palindrome =====");
+		}else{
+			System.out.println("==== "+teks+" bukan Palindrome =====");
+		}
+	}
+}
+
